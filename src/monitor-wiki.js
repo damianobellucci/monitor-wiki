c8960211523d85d400e270aba0c5affa7cd0ba9f
damianobellucci <damiano.bellucci@studio.unibo.it>
2019-01-24 20:26:12 +0100

    Improvements

--- src/monitor-wiki.js
+++ src/monitor-wiki.js
@@ -1,7 +1,6 @@
 var functions = require('./functions.js');
 var wrappersModality = require('./wrappersModality.js');
 var fs = require('fs');
-var wrapper = require('./wrappers.js');
 
 
 //MAIN
@@ -51,7 +50,67 @@ var wrapper = require('./wrappers.js');
             fs.writeFile(parsedRequest.d, JSON.stringify(finalExport), function (err) {
                 if (err) throw err;
                 console.log('Export completed');
-                
+
+                //console.log('Time elapsed for export: ' + resultInfo.timer / 1000 + 's');
+            });
+        }
+        else if (parsedRequest.m === 'aggregateInfo') {
+
+            if (!parsedRequest.f) { console.log('Error (input): missing input file.'); return; }
+            //if (new Date(timespanArray[0]) > new Date(timespanArray[1])) { console.log('Error (timespan): ' + parsedRequest.t + ' is an invalid timespan.'); return };
+            //if (isNaN(parsedRequest.n) || parsedRequest.n < 0) { console.log('Error (nEditCriteria): ' + parsedRequest.n + ' is not a valid nEditCriteria'); return; };
+            //if (isNaN(parsedRequest.f) || parsedRequest.f < 0) { console.log('Error (frequencyEditCriteria): ' + parsedRequest.f + ' is not a valid frequencyEditCriteria'); return; };
+
+            let finalExport = { query: parsedRequest, result: {} };
+            let stringParsedRequest = JSON.stringify(parsedRequest);
+
+
+
+            for (let i = 0; i < Object.keys(parsedRequest.t).length; i++) {
+                let params = JSON.parse(stringParsedRequest);
+                params.t = params.t[i];
+                finalExport.result[i] = await Promise.resolve(wrappersModality.Info(params));
+            }
+
+            let aggregatedExport = { query: parsedRequest, result: {} };
+
+
+            Object.keys(finalExport.result).forEach((resultPage) => {
+                aggregatedResultPage = { timespan: parsedRequest.t[resultPage], pages: {} };
+                Object.keys(finalExport.result[resultPage].pages).forEach((page) => { //qui aggrego
+                    let aggregatedPage = {
+                        pageid: finalExport.result[resultPage].pages[page].pageid,
+                        title: finalExport.result[resultPage].pages[page].title,
+                        daysOfAge: finalExport.result[resultPage].pages[page].daysOfAge
+                    };
+
+                    //aggrego il numero di revisioni (utenti,minor edits)
+
+                    if (finalExport.result[resultPage].pages[page].hasOwnProperty('revisions')) {
+                        aggregatedPage.edits = finalExport.result[resultPage].pages[page].revisions.history.length;
+                        aggregatedPage.minorEdits = finalExport.result[resultPage].pages[page].revisions.history.filter(el => { return el.hasOwnProperty('minor') }).length;
+                        aggregatedPage.authors = Array.from(new Set(finalExport.result[resultPage].pages[page].revisions.history.map(el => el.user))).length;
+                    }
+                    //aggrego numero di commenti
+                    finalExport.result[resultPage].pages[page].hasOwnProperty('talks') ?
+                        aggregatedPage.comments = finalExport.result[resultPage].pages[page].talks.history.length : null;
+
+                    //aggrego views
+
+                    finalExport.result[resultPage].pages[page].views === 'Not Available' ?
+                        aggregatedPage.views = 'n/a' : aggregatedPage.views = finalExport.result[resultPage].pages[page].views.map(el => el.views).reduce((a, b) => a + b, 0);
+
+                    aggregatedResultPage.pages[aggregatedPage.pageid] = aggregatedPage;
+                    console.log(aggregatedPage);
+
+                });
+                aggregatedExport.result[resultPage] = aggregatedResultPage;
+            })
+
+            fs.writeFile(parsedRequest.d, JSON.stringify(aggregatedExport), function (err) {
+                if (err) throw err;
+                console.log('Export completed');
+
                 //console.log('Time elapsed for export: ' + resultInfo.timer / 1000 + 's');
             });
         }
